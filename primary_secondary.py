269ad5d2ee834e8204b28210a98c94422a6e0730
Erin Casseapey <friskytenno@gmail.com>
2017-07-15 22:31:30 +0800

    init

---
+++ primary_secondary.py
@@ -0,0 +1,16 @@
+## this program is similar to the vowels/consonants activity.
+
+color=input("Type a color to determine if it's primary, secondary or tertiary: ")
+
+if(color=="red") or (color=="blue") or (color=="yellow"):
+    print("This is a primary color.")
+elif(color=="green") or (color=="purple") or (color=="orange"):
+    print("This is a secondary color.")
+elif(color=="marigold") or (color=="vermilion") or (color=="teal") or (color=="lime"):
+    print("This is a tertiary color.")
+## this is optional
+else:
+    print("I don't recognize that color.")
+
+## another optional thing
+exit()
