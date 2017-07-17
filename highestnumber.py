e4bec0203c19f5e9204a41de530f93fa46de565f
Erin Casseapey <friskytenno@gmail.com>
2017-07-17 18:51:26 +0800

    update - possible test for wednesday
    So a certain someone informed me that this is a possible test for wednesday - a certain program that determines what is the highest number, and how it reacts to inputs that are equal to each other. Read and test at your own risk. Any questions, read the readme (and FAQ.). Good evening.

---
+++ highestnumber.py
@@ -0,0 +1,27 @@
+number1=int(input("Please enter the first number: "))
+number2=int(input("Please enter the second number: "))
+number3=int(input("Please enter the third number: "))
+
+if (number1>number2) and (number1>number3):
+    print("The highest number is",number1)
+
+elif (number2>number1) and (number2>number3):
+    print("The highest number is",number2)
+
+elif (number3>number1) and (number3>number2):
+    print("The highest number is",number3)
+
+elif (number1>number2) and (number1==number3):
+    print("Both the first and third number - which is",number1,"- are the highest.")
+
+elif (number2>number1) and (number2==number3):
+    print("Both the second and third number - which is",number2,"- are the highest.")
+
+elif (number2>number3) and (number2==number1):
+    print("Both the first and second number - which is",number2,"- are the highest.")
+
+elif (number1==number2) and (number1==number3):
+    print("Woah there, you typed the same number three times in a row - you sure 'bout that?")
+
+else:
+    print("Can't compute.")
